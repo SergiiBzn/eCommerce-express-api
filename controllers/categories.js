926e70344243f783ef533dafd4694c5c5876ce86
Alexander Bashenow <bashenow.alexander@gmail.com>
2025-08-27 13:30:43 +0200

    controllers added

---
+++ controllers/categories.js
@@ -0,0 +1,66 @@
+export const getCategory = async (req, res) => {
+  try {
+    const category = await Category.findAll();
+    res.json(category);
+  } catch (error) {
+    res.status(500).json({ error: error.message });
+  }
+};
+
+export const createCategory = async (req, res) => {
+  try {
+    const {
+      body: { name },
+    } = req;
+    if (!name) throw new Error("Name for category required");
+    const found = await Category.findOne({ where: { name } });
+    if (found) throw new Error("Category already exists");
+    const category = await Category.create(req.body);
+    res.json(category);
+  } catch (error) {
+    res.status(500).json({ error: error.message });
+  }
+};
+
+export const getCategoryById = async (req, res) => {
+  try {
+    const {
+      params: { id },
+    } = req;
+    const category = await Category.findByPk(id);
+    if (!category) return res.status(404).json({ error: "Category not found" });
+    res.json(category);
+  } catch (error) {
+    res.status(500).json({ error: error.message });
+  }
+};
+
+export const updateCategory = async (req, res) => {
+  try {
+    const {
+      body: { name },
+      parama: { id },
+    } = req;
+    if (!name) throw new Error("Name of category required");
+    const category = await Category.findByPk(id);
+    if (!category) return res.status(404).json({ error: "Category not found" });
+    await category.update(req.body);
+    res.json(category);
+  } catch (error) {
+    res.status(500).json({ error: error.message });
+  }
+};
+
+export const deleteCategory = async (req, res) => {
+  try {
+    const {
+      params: { id },
+    } = req;
+    const category = await Category.findByPk(id);
+    if (!category) return res.status(404).json({ error: "Category not found" });
+    await category.destroy();
+    res.json({ message: "Category deleted" });
+  } catch (error) {
+    res.status(500).json({ error: error.message });
+  }
+};
---
+++ controllers/products.js
@@ -0,0 +1,69 @@
+export const getProducts = async (req, res) => {
+  try {
+    const products = await Product.findAll();
+    res.json(products);
+  } catch (error) {
+    res.status(500).json({ error: error.message });
+  }
+};
+
+export const createProduct = async (req, res) => {
+  try {
+    const {
+      body: { name, description, price },
+    } = req;
+    if (!name || !description || !price)
+      throw new Error("Name, description and price are required");
+    const found = await Product.findOne({ where: { name } });
+    if (found) throw new Error("Product already exists");
+    const product = await Product.create(req.body);
+    res.json(product);
+  } catch (error) {
+    res.status(500).json({ error: error.message });
+  }
+};
+
+export const getProductById = async (req, res) => {
+  try {
+    const {
+      params: { id },
+    } = req;
+    const product = await Product.findByPk(id);
+    if (!product)
+      return res.status(404).json({ error: `Product with ID:${id} not found` });
+    res.json(product);
+  } catch (error) {
+    res.status(500).json({ error: error.message });
+  }
+};
+
+export const updateProduct = async (req, res) => {
+  try {
+    const {
+      body: { name, description, price },
+      params: { id },
+    } = req;
+    if (!name || !description || !price)
+      throw new Error("Name, description and price are required");
+    const product = await Product.findByPk(id);
+    if (!product) return res.status(404).json({ error: `Product not found` });
+    await product.update(req.body);
+    res.json(product);
+  } catch (error) {
+    res.status(500).json({ error: error.message });
+  }
+};
+
+export const deleteProduct = async (req, res) => {
+  try {
+    const {
+      params: { id },
+    } = req;
+    const product = await Product.findByPk(id);
+    if (!product) return res.status(404).json({ error: `Product not found` });
+    await product.destroy();
+    res.json({ message: "Product deleted" });
+  } catch (error) {
+    res.status(500).json({ error: error.message });
+  }
+};
